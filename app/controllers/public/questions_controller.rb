b8106013bee75d5653b794a0361ba558056ba303
EC2 Default User <ec2-user@ip-172-31-18-196.ap-northeast-1.compute.internal>
2023-01-29 16:05:16 +0000

    [Create]質問一覧画面作成

--- app/controllers/public/questions_controller.rb
+++ app/controllers/public/questions_controller.rb
@@ -1,13 +1,43 @@
 class Public::QuestionsController < ApplicationController
   def new
+    @question = Question.new
+  end
+  
+  def create
+    @question = Question.new(question_params)
+    @question.user_id = current_user.id
+    if @queston.save
+      redirect_to questions_path, notice:"質問が投稿されました。"
+    else
+      render :new
+    end  
   end
 
   def index
+    @questions = Question.all
   end
 
   def show
+     @question = Question.find(params[:id])
   end
 
   def edit
+     @question = Question.find(params[:id])
+    
+  end
+  
+  def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+     redirect_to question_path(@question.id), notice:"質問内容を編集しました。"
+    else
+     render :edit
+    end
+  end  
+  
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to '/questions'
   end
 end
