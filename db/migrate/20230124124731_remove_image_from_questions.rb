393d22a357b5ed359b77d66d36c9b0e824e2eaac
tetsuya3656 <nycticebus01_coucang10@yahoo.co.jp>
2023-02-11 11:41:49 +0000

    [Edit]migrationファイル削除

--- db/migrate/20230124124731_remove_image_from_questions.rb
+++
@@ -1,5 +0,0 @@
-class RemoveImageFromQuestions < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :questions, :image, :string
-  end
-end
--- db/migrate/20230124124836_remove_image_from_post_images.rb
+++
@@ -1,5 +0,0 @@
-class RemoveImageFromPostImages < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :post_images, :image, :string
-  end
-end
